0f0f16713c8e0b316cba0e2246c2e917d02fb30e
wei <891001kevin@gmail.com>
2024-03-07 11:26:35 +0800

    Return&ReturnDetail Entity

---
+++ src/main/java/com/willy/malltest/repository/ReturnDetailsRepository.java
@@ -0,0 +1,6 @@
+package com.willy.malltest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReturnDetailsRepository extends JpaRepository<ReturnDetails,Integer> {
+}
---
+++ src/main/java/com/willy/malltest/repository/ReturnsRepository.java
@@ -0,0 +1,6 @@
+package com.willy.malltest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReturnsRepository extends JpaRepository<Returns,Integer> {
+}
