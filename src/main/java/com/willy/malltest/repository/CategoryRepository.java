0dcaf8240ec709a56f96b1b084ef476530dbb0c7
wei <891001kevin@gmail.com>
2024-03-07 11:50:59 +0800

    package fixed

--- src/main/java/com/willy/malltest/repository/CategoryRepository.java
+++ src/main/java/com/willy/malltest/repository/CategoryRepository.java
@@ -1,5 +1,6 @@
 package com.willy.malltest.repository;
 
+import com.willy.malltest.model.CartItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<CartItems, String> {
