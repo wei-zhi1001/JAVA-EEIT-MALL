3088ac15170b247fac04f5ab7a92a0f3f56b4ef1
willy <willyjen0530@gmail.com>
2024-03-27 16:32:04 +0800

    CrossOrigin

--- src/main/java/com/willy/malltest/controller/CustomerFeedBackController.java
+++ src/main/java/com/willy/malltest/controller/CustomerFeedBackController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class CustomerFeedBackController {
 
     @Autowired
--- src/main/java/com/willy/malltest/controller/TrackController.java
+++ src/main/java/com/willy/malltest/controller/TrackController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class TrackController {
 
     @Autowired
